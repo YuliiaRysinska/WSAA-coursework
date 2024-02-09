302b48535310d189ffde96c78ca3a6e1ed907b37
Админ <iuy.nickolaencko@gmail.com>
2024-02-09 12:21:34 +0000

    request

--- labs/traintimes.py
+++ labs/traintimes.py
@@ -1,13 +1,4 @@
-#program that prints the data for all trains to console.
-# !!!!You can modify this to store all the data in a CSV file
-# Use API http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML to retrieve the data.
-
-# Then as an exercise only store trains that have a train code that starts with D:
-# For data sets of this size I would normally get all the data, and perform analysis (deletions) later.
-# Get the data:
-# 1. Go to the URL and check that it works, have a quick look at the format of the XML.
-# 2. Create a python program that reads the XML from the URL and prints it out, using minidom. Check it does retrieve the data
-
+# !!!!!!!! again don't works!!!!!!!!
 from xml.dom.minidom import parseString
 import requests
 import csv
@@ -26,11 +17,12 @@ page = requests.get(url)
 
 doc = parseString(page.content)
 # check it works
-print (doc.toprettyxml(), end='') #output to console
+# newl='' gets rid of the new lines when printing
+print (doc.toprettyxml(newl='')) #output to console
 
 # if I want to store the xml in a file
-# with open("trainxml.xml","w") as xmlfp:
-#   doc.writexml(xmlfp)
+with open("trainxml.xml","w") as xmlfp:
+    doc.writexml(xmlfp)
 
 
 # I had an issue with blank lines in the file so found solution at
@@ -41,9 +33,9 @@ with  open('week02_train.csv', mode='w', newline='') as train_file:
 
     objTrainPositionsNodes = doc.getElementsByTagName("objTrainPositions")
     for objTrainPositionsNode in objTrainPositionsNodes:
-        traincodenode = objTrainPositionsNode.getElementsByTagName("TrainCode").item(0)
-        traincode = traincodenode.firstChild.nodeValue.strip()
-        print (traincode)
+        #traincodenode = objTrainPositionsNode.getElementsByTagName("TrainCode").item(0)
+        #traincode = traincodenode.firstChild.nodeValue.strip()
+        #print (traincode)
 
         # now lets get everything
         dataList = []
@@ -55,8 +47,3 @@ with  open('week02_train.csv', mode='w', newline='') as train_file:
         #print (dataList)
         # for example a CSV file  
         train_writer.writerow(dataList)
-
- 
-
-
-
