8afc3be5e95c08996b8b192bb83552f271199b0b
Админ <iuy.nickolaencko@gmail.com>
2024-02-19 14:41:50 +0000

    +

---
+++ assignments/assignment03/cso.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+urlBegining = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/"
+urlEnd = "/JSON-stat/2.0/en"
+def getAll(dataset):
+    url = urlBegining + dataset + urlEnd
+    response = requests.get(url)
+    return response.json()
+
+if __name__ =="__main__":
+    with open("cso.json", "wt") as fp:
+        print(json.dumps(getAll("FIQ02")), file=fp)
