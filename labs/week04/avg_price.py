bbc497adb56683ea9368f8b6d20b431405f6b333
Админ <iuy.nickolaencko@gmail.com>
2024-02-26 18:35:43 +0000

    +

--- labs/week04/avg_price.py
+++ labs/week04/avg_price.py
@@ -1,9 +1,12 @@
 #Write a program in another file that works out the average book price from all the books on the server
-from my_api_vsc import readBoks
+from my_api_vsc import readBooks
 
 books = readBooks()
 
 total = 0
 count = 0
-for book in books:    
-    print(book))
+for book in books:  
+    total +=book["Price"]
+    count += 1
+      
+print('Average price of', count, 'books is', total/count)
