44bad742c0815e8ca9b6546e9b19e54194225e15
Админ <iuy.nickolaencko@gmail.com>
2024-03-02 23:32:22 +0000

    read file from other publick repo

--- assignments/assignment04/github.py
+++ assignments/assignment04/github.py
@@ -7,9 +7,8 @@ import json
 #filename = "repos-public.json"
 filename = "wsaa-code.json"
 
-url = 'https://api.github.com/users/andrewbeattycourseware/repos?per_page=100'
+
 url = 'https://api.github.com/repos/andrewbeattycourseware/wsaa-course-material/contents/code'
-#url = 'https://api.github.com/repos/andrewbeattycourseware/aprivateone'
 
 response = requests.get(url)
 print (response.status_code)
@@ -18,3 +17,4 @@ repoJSON = response.json()
 
 with  open(filename, 'w') as fp:
     json.dump(repoJSON, fp, indent=4)
+    
--- labs/week05/packages/githubbymodule.py
+++ labs/week05/packages/githubbymodule.py
@@ -1,26 +1,2 @@
-# 1. 1. install pyGithub: pip install PyGithub
-#2. Test that your pyGithub works
 
-# ok this is not exactly like I asked you to to in the labs
-import requests
-import json
-from config import config as cfg
 
-filename = "repos-private.json"
-
-#url = 'https://api.github.com/repos/andrewbeattycourseware/datarepresentation/contents/code'
-url = 'https://api.github.com/repos/andrewbeattycourseware/aprivateone'
-
-# the more basic way of setting authorization
-#headers = {'Authorization': 'token ' + apikey}
-#response = requests.get(url, headers= headers)
-
-apikey = cfg["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"]
-response = requests.get(url, auth = ('token', apikey))
-
-print (response.status_code)
-#print (response.json())
-
-with  open(filename, 'w') as fp:
-    repoJSON = response.json()
-    json.dump(repoJSON, fp, indent=4)
