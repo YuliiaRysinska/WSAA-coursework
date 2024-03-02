a4e0d9ec4a558d96ad6eb621e85e2d068cedf8cc
Админ <iuy.nickolaencko@gmail.com>
2024-03-02 22:40:22 +0000

    prog read  file from WSAA repo

--- assignments/assignment04/github.py
+++ assignments/assignment04/github.py
@@ -1,2 +1,20 @@
 # program read a file from a repository, replace all the instances of the text "Andrew" with your name. 
 # program should then commit those changes and push the file back to the repository (You will need authorisation to do this).
+
+import requests
+import json
+
+#filename = "repos-public.json"
+filename = "wsaa-code.json"
+
+url = 'https://api.github.com/users/andrewbeattycourseware/repos?per_page=100'
+url = 'https://api.github.com/repos/andrewbeattycourseware/wsaa-course-material/contents/code'
+#url = 'https://api.github.com/repos/andrewbeattycourseware/aprivateone'
+
+response = requests.get(url)
+print (response.status_code)
+repoJSON = response.json()
+#print (response.json())
+
+with  open(filename, 'w') as fp:
+    json.dump(repoJSON, fp, indent=4)
