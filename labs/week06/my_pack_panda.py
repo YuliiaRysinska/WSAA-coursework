d8bdc9a4aab04d9bcd399651d1599be1e53ec657
Админ <iuy.nickolaencko@gmail.com>
2024-03-03 18:11:07 +0000

    Doc(RESR API)/venv/flask

---
+++ labs/week06/my_pack_panda.py
@@ -0,0 +1,10 @@
+# 1. Creat a REST API for an entity of your choice.
+import pandas as pd
+print("Yuliasha")
+
+#2. Create a virtual environment to run a Flask server
+#3. Create a directory where this server is going to run . Create a Virtual environment:python -m venv venv
+#4.Run the virtual environment: .\venv\Scripts\activate.bat
+#5. Install Flask: Pip install flask
+# 6.Save the package list in a file called requirements.txt: pip freeze > requirements.txt
+# 8. Create a file called rest_server.py.......
