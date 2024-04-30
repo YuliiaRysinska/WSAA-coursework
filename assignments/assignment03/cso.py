bb1b014c3090ad52281cb19701b52c56bb6b3ced
Админ <iuy.nickolaencko@gmail.com>
2024-04-30 15:30:54 +0100

    update files

--- assignments/assignment03/cso.py
+++ assignments/assignment03/cso.py
@@ -1,6 +1,8 @@
+
 import requests
 import json
 
+#get specific data
 urlBegining = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/"
 urlEnd = "/JSON-stat/2.0/en"
 def getAll(dataset):
@@ -8,6 +10,7 @@ def getAll(dataset):
     response = requests.get(url)
     return response.json()
 
+# create this json file in VSC
 if __name__ =="__main__":
-    with open("cso.json", "wt") as fp:
+    with open("cso_data.json", "wt") as fp:
         print(json.dumps(getAll("FIQ02")), file=fp)
